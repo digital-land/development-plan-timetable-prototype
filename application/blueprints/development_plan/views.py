5e7a07d3b984daaccb502a6bb5b989eefd34a5f9
Colm Britton <colmjude@gmail.com>
2023-04-14 15:31:22 +0100

    Add basic form for adding/editing plan record

--- application/blueprints/development_plan/views.py
+++ application/blueprints/development_plan/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+from application.blueprints.development_plan.forms import PlanForm
 from application.models import DevelopmentPlan
 
 development_plan = Blueprint(
@@ -11,3 +12,30 @@ development_plan = Blueprint(
 def plan(reference):
     development_plan = DevelopmentPlan.query.get(reference)
     return render_template("plan.html", development_plan=development_plan)
+
+
+@development_plan.route("/<string:reference>/edit")
+def edit(reference):
+    development_plan = DevelopmentPlan.query.get(reference)
+
+    form = PlanForm()
+    if form.validate_on_submit():
+        # handle form
+        pass
+
+    return render_template(
+        "plan/edit.html", development_plan=development_plan, form=form
+    )
+
+
+@development_plan.route("/add")
+def new():
+    # check if creating record for a given LPA
+    # requests.get...
+
+    form = PlanForm()
+    if form.validate_on_submit():
+        # handle form
+        pass
+
+    return render_template("plan/new.html", form=form)

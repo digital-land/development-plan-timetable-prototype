245075786a31294fe338f4c8568758f7f217bcf0
Colm Britton <colmjude@gmail.com>
2023-04-19 12:04:34 +0100

    Add forms for adding records to registers

---
+++ application/blueprints/admin/forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class EventForm(FlaskForm):
+    name = StringField(
+        "Name",
+        validators=[DataRequired()],
+        description="Give the event a recognisable name, for example, Public consulation start",
+    )
+    reference = StringField(
+        "Reference",
+        validators=[DataRequired()],
+        description="Add a reference in kebab case format, for example, public-consultation-start",
+    )
+    description = TextAreaField("Description")
+
+
+class PlanTypeForm(FlaskForm):
+    name = StringField(
+        "Name",
+        validators=[DataRequired()],
+        description="Give the plan type a recognisable name, for example, Local plan",
+    )
+    reference = StringField(
+        "Reference",
+        validators=[DataRequired()],
+        description="Add a reference in kebab case format, for example, local-plan",
+    )
+    description = TextAreaField("Description")
+
+
+class DocumentTypeForm(FlaskForm):
+    name = StringField(
+        "Name",
+        validators=[DataRequired()],
+        description="Give the document type a recognisable name, for example, Inspectors report",
+    )
+    reference = StringField(
+        "Reference",
+        validators=[DataRequired()],
+        description="Add a reference in kebab case format, for example, inspectors-report",
+    )
+    description = TextAreaField("Description")
--- application/blueprints/admin/views.py
+++ application/blueprints/admin/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+from application.blueprints.admin.forms import DocumentTypeForm, EventForm, PlanTypeForm
 from application.models import DevelopmentPlanEvent, DevelopmentPlanType, DocumentType
 
 admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
@@ -16,13 +17,47 @@ def events():
     return render_template("admin/events.html", events=evts)
 
 
+@admin_bp.route("/events/add")
+def add_event():
+    form = EventForm()
+
+    if form.validate_on_submit():
+        pass
+
+    return render_template("admin/add-record.html", register_name="event", form=form)
+
+
 @admin_bp.route("/document-types")
 def document_types():
     document_types = DocumentType.query.all()
     return render_template("admin/document-types.html", document_types=document_types)
 
 
+@admin_bp.route("/document-types/add")
+def add_document_type():
+    form = DocumentTypeForm()
+
+    if form.validate_on_submit():
+        pass
+
+    return render_template(
+        "admin/add-record.html", register_name="document_type", form=form
+    )
+
+
 @admin_bp.route("/plan-types")
 def plan_types():
     plan_types = DevelopmentPlanType.query.all()
     return render_template("admin/plan-types.html", plan_types=plan_types)
+
+
+@admin_bp.route("/plan-types/add")
+def add_plan_type():
+    form = PlanTypeForm()
+
+    if form.validate_on_submit():
+        pass
+
+    return render_template(
+        "admin/add-record.html", register_name="plan_type", form=form
+    )

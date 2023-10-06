6b2b579d49649cded39a649f4f68bb7584aeb821
colmjude <colmjude@gmail.com>
2023-10-06 17:08:58 +0300

    Add basic filter for planning authorities with or without adopted plans

--- application/blueprints/organisation/views.py
+++ application/blueprints/organisation/views.py
@@ -19,13 +19,25 @@ def organisations():
         )
 
     # TODO: how do I get all orgs that should be publishing a plan?
-    orgs = get_organisations_expected_to_publish_plan()
+    all_orgs = get_organisations_expected_to_publish_plan()
     adopted_plans, orgs_with_adopted_plan = get_adopted_plans()
+
+    orgs = all_orgs
+    if request.args.get("planningAuthorityFilter"):
+        if request.args.get("planningAuthorityFilter") == "with":
+            orgs = orgs_with_adopted_plan
+        elif request.args.get("planningAuthorityFilter") == "without":
+            org_ids_to_remove = {org.organisation for org in orgs_with_adopted_plan}
+            orgs = [
+                org for org in all_orgs if org.organisation not in org_ids_to_remove
+            ]
+
     return render_template(
         "organisation/index.html",
         organisations=orgs,
         adopted_plans=adopted_plans,
         orgs_with_adopted_plan=orgs_with_adopted_plan,
+        planning_authority_filter=request.args.get("planningAuthorityFilter"),
     )
 
 
